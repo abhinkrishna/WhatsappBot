ab3bd44d1a693b03032568bc593eb21b04433035
Abhin Krishna <abhinkrishna.ka@gmail.com>
2019-08-25 14:37:19 +0530

    Error Resume...
    Status : Stable
    -Resume even after an error occured.

--- app.py
+++ app.py
@@ -7,40 +7,48 @@ class Whatsapp_Bot:
         print("Initializing Whatapp_Bot...")
         self.to = recipient
         self.rep = rep
-        self.msg = '#    ' + msg
+        self.msg = '#  ' + msg
         self.bot = webdriver.Chrome(executable_path="drivers/chromedriver.exe")
-    def auth(self):
+        self.count = 0
+
+    def start_service(self):
         print('opening browser...')
         browser = self.bot
         browser.get("https://web.whatsapp.com")
-        time.sleep(8)
-        print('Done')
         res = input("Scan QR Code to login and press enter key to continue...")
         if(res==''):
-            print("continuing...")
+            print("Searching recipient...")
             searchbox = browser.find_element_by_class_name('_2zCfw') #searchBox
             searchbox.clear()
             searchbox.send_keys(self.to)
             searchbox.send_keys(Keys.RETURN)
             time.sleep(3)
-            body = browser.find_element_by_tag_name("body") 
-            for i in range(0,self.rep):
-                try:
-                    body.send_keys(self.msg)
-                    btn = browser.find_element_by_class_name("_3M-N-")
-                    btn.click()
-                    time.sleep(0.5)
-                except:
-                    print(i,"Messages sended Successfully,",self.rep-i,"Missed.")
-                    break
-                print(i+1) # Couting             
+            def send(number):
+                i = 0
+                while(i<number):
+                    i+=1
+                    try:
+                        body = browser.find_element_by_tag_name("body")
+                        body.send_keys(self.msg)
+                        btn = browser.find_element_by_class_name("_3M-N-")
+                        btn.click()
+                        self.count += 1
+                        print(self.count) # Couting
+                        time.sleep(1)
+                    except:
+                        print(self.count,"Messages sended Successfully,",self.rep-self.count,"Missed.")
+                        print("Retrying... Starting form",self.count)
+                        send(self.rep - self.count)
+                        break                    
+            send(self.rep)  #Calling for initial state...
         else:
             print("Okay Exiting...")
             browser.close()
 
 to = input("To : ")
 msg = input("Message : ")
-reps = int(input("No. of repetitions :"))
-app = Whatsapp_Bot(to,reps,msg) # parameters (<recipient>)
-app.auth()
+reps = int(input("No. of repetitions : "))
+
+app = Whatsapp_Bot(to,reps,msg) # parameters (<recipient>,<repetitions>,<message>)
+app.start_service()
 input()
